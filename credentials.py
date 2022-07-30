b7f65ad4506496497392bdbd5206664c76cf673e
TeamSoulMortal <110298811+TeamSoulMortal@users.noreply.github.com>
2022-07-30 21:32:54 +0530

    Tweepy : Create Credentials.py

---
+++ credentials.py
@@ -0,0 +1,8 @@
+# Get your keys from Twitter Developer website and paste 
+# it accordingly here in the file
+# This file will be used as a credentials file by the main script
+
+CONSUMER_KEY = 'Your consumer key here'
+CONSUMER_KEY_SECRET = 'Your consumer secrets here'
+ACCESS_TOKEN = 'Your access token here'
+ACCESS_TOKEN_SECRET = 'Your access token secret here'
